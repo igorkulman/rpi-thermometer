73957932ea2bf79dfb1e01716e7898247b1b428f
Igor Kulman <igor@kulman.sk>
2013-12-06 21:14:43 +0000

    + express added

--- server.js
+++ server.js
@@ -1,32 +1,22 @@
-var http = require("http");
+var express = require("express");
 var fs = require("fs");
 var exec = require('child_process').exec;
 
 console.log("Starting");
 
-var server = http.createServer(function(request, response) {
-	console.log("Received request: "+request.url);
-	var filename = request.url;
-	if (filename=="/"){
-		filename="/index.html";
-	}
-	fs.readFile("./public"+filename, function(error, data){
-		if (error){
-			response.writeHead(404);
-			response.end();
-		} else {
-			response.writeHead(200);
-			if (filename=="/index.html"){
-				 var child = exec("cat ./fake.data", function (error, stdout, stderr) {
-				 	var tempData = stdout.toString().split('\n')[1];
-				 	var temp = parseInt(tempData.split('=')[1]) / 1000;
-				 	response.end(data.toString().replace("TEMP",temp));
-				});
-			} else {
-				response.end(data);
-			}
-		}
-	});
+var app = express.createServer();
+
+app.use(app.router);
+app.use(express.static(__dirname+"/public"));
+
+app.get("/", function(request, response){
+    fs.readFile("./public"+filename, function(error, data){
+        var child = exec("cat ./fake.data", function (error, stdout, stderr) {
+            var tempData = stdout.toString().split('\n')[1];
+            var temp = parseInt(tempData.split('=')[1]) / 1000;
+            response.send(data.toString().replace("TEMP",temp));
+        });
+    });
 });
 
-server.listen(8000);
+app.listen(8000);

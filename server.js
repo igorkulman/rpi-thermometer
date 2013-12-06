d88a503767a709bb50b231517f790c0fcb7a464f
Igor Kulman <igor@kulman.sk>
2013-12-06 21:18:42 +0000

    + working express solution

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var exec = require('child_process').exec;
 
 console.log("Starting");
 
-var app = express.createServer();
+var app = express();
 
 app.use(app.router);
 app.use(express.static(__dirname+"/public"));
 
 app.get("/", function(request, response){
-    fs.readFile("./public"+filename, function(error, data){
+    fs.readFile("./public/index.html", function(error, data){
         var child = exec("cat ./fake.data", function (error, stdout, stderr) {
             var tempData = stdout.toString().split('\n')[1];
             var temp = parseInt(tempData.split('=')[1]) / 1000;

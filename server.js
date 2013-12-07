bb455e41d77b50008510c803170b458ae9e09419
Igor Kulman <igor@kulman.sk>
2013-12-07 12:30:25 +0000

    + using dust template engine

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 var express = require("express");
 var fs = require("fs");
-var exec = require('child_process').exec;
+var exec = require("child_process").exec;
+var cons = require('consolidate')
 
 console.log("Starting");
 
 var app = express();
+app.engine('dust',cons.dust);
 
 app.use(app.router);
 app.use(express.static(__dirname+"/public"));
+app.set('view engine', 'dust');
+app.set('views', __dirname + '/views');
 
 app.get("/", function(request, response){
-    fs.readFile("./public/index.html", function(error, data){
         var child = exec("cat ./fake.data", function (error, stdout, stderr) {
             var tempData = stdout.toString().split('\n')[1];
             var temp = parseInt(tempData.split('=')[1]) / 1000;
-            response.send(data.toString().replace("TEMP",temp));
+            response.render('index', {
+		    temperature: temp
+	     });
         });
-    });
 });
 
 app.listen(8000);

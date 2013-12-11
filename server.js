117c3e7f7f6eecc5125b196c4216ce5bcff3d26f
Igor Kulman <igor@kulman.sk>
2013-12-11 10:22:38 +0100

    * quotes and apostrophes consolidated

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 var express = require("express");
-var fs = require("fs");
 var exec = require("child_process").exec;
-var cons = require('consolidate')
+var cons = require("consolidate");
 
 var deviceId="28-000004e23e98";
 
 console.log("Starting");
 
 var app = express();
-app.engine('dust',cons.dust);
+app.engine("dust",cons.dust);
 
 app.use(app.router);
 app.use(express.static(__dirname+"/public"));
-app.set('view engine', 'dust');
-app.set('views', __dirname + '/views');
+app.set("view engine", "dust");
+app.set("views", __dirname + "/views");
 
 app.get("/", function(request, response){
         	getTemperature(function(temp){
-        		response.render('index', {
+        		response.render("index", {
 		    		temperature: temp
 	     		});
         	});
@@ -33,7 +32,7 @@ app.get("/temperature", function(request, response){
 function getTemperature(callback){
 	var child = exec("cat /sys/bus/w1/devices/"+deviceId+"/w1_slave", function (error, stdout, stderr) {
             var tempData = stdout.toString().split('\n')[1];
-            var temp = parseInt(tempData.split('=')[1]) / 1000;
+            var temp = parseInt(tempData.split("=")[1]) / 1000;
            	callback(temp);
            });
 }

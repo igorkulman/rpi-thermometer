e7500f74cada15d08cb9f5ed4f95ae6fa6c9a213
Igor Kulman <igor@kulman.sk>
2013-12-11 08:29:34 +0000

    + thermometer gauge height set according to temperature value

--- public/refresh.js
+++ public/refresh.js
@@ -1,7 +1,10 @@
+updateThermometer(document.getElementById("temp").innerHTML);
+
 xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             document.getElementById("temp").innerHTML = xmlhttp.responseText;
+	    updateThermometer(xmlhttp.responseText);
         }
     };
 
@@ -9,3 +12,10 @@ setInterval(function() {
                     xmlhttp.open("GET", "/temperature", true);
                     xmlhttp.send();
                 }, 30000);
+
+function updateThermometer(temp)
+{
+	var val = Math.round(100-2*temp);
+	document.getElementById("t").style.background = "-webkit-linear-gradient(top, #fff 0%, #fff "+val+"%, #db3f02 "+val+"%, #db3f02 100%)";
+	document.getElementById("temp").innerHTML = temp;
+}

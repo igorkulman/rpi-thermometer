5dea4364a210484f453f9d291cf0c2087b06126a
Igor Kulman <igor@kulman.sk>
2015-06-12 18:11:10 +0000

    * fetching history data from SynergyKit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var deviceId = config.deviceId;
 var port = config.port;
 
 Synergykit.Init(config.synergyKitAppUrl, config.synergyKitKey, {
-    debug: true // You should set it to false in production
+    debug: false // You should set it to false in production
 });
 
 console.log("Starting");
@@ -36,19 +36,45 @@ app.get("/temperature", function (request, response) {
 });
 
 app.get("/history", function (request, response) {    
-    
+    var interval = request.param('timeinterval');
+    if (interval == undefined){
+        interval="24";
+    }
+
+    var temps = Synergykit.Data("Temperature");
+    var query = Synergykit.Query(temps);
+
+    var myDate = new Date();
+    myDate.setHours(myDate.getHours() - parseInt(interval));
+
+    query.where()
+    .attribute("createdAt")
+    .isGreaterThan(myDate.getTime())
+    .find({
+	    success: function(rows, statusCode) {
+        	 response.render("history", {
+         	    data: rows,
+	            is24: interval == "24",
+		    is6:  interval == "6",
+        	    is12: interval == "12",
+	            is168: interval == "168",
+        	    is720: interval == "720"
+        	})
+	    },
+	    error: function(error, statusCode) {
+        	response.send("error")
+	    }
+	})
 });
 
 app.get("/measure", function (request, response) {
 
     getTemperature(function (temp) {
-        var record = Synergykit.Data("Temperature");
-	record.set("Timestamp", new Date().getTime());
-	record.set("Value", temp);
+        var record = Synergykit.Data("Temperature");	
+	record.set("value", temp);
 
 	record.save({
-    success: function(spaceShip, statusCode) {
-        // Your implementation after the space ship is saved
+    success: function(spaceShip, statusCode) {       
       	response.send("ok: "+temp.toString());
     },
     error: function(error, statusCode) {

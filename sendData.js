4248d44faa73d017171b6f18fe31518c323f5c2c
Igor Kulman <igor@kulman.sk>
2015-06-18 12:16:59 +0000

    + separate file for just sending the data to SynergyKit, suitable for calling from cron

---
+++ sendData.js
@@ -0,0 +1,26 @@
+var fs = require("fs");
+var Synergykit = require("synergykit");
+var rpiTemp = require('rpi-temp-module');
+
+var config = JSON.parse(fs.readFileSync("config.json"));
+var deviceId = config.deviceId;
+
+Synergykit.Init(config.synergyKitAppUrl, config.synergyKitKey, {
+    debug: false 
+});
+
+rpiTemp.getTemperature(deviceId, function(temp) {
+        var record = Synergykit.Data("Temperature");
+        record.set("value", temp);
+
+        record.save({
+            success: function(data, statusCode) {
+                console.log("ok: " + temp.toString());
+            },
+            error: function(error, statusCode) {
+                console.log("fail");
+            }
+        });
+
+
+    });
